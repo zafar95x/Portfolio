a72a5dd5882b1d6084ce551601b7c68f0de4f5ab
Zafar Mahmood <zafar@Zafars-MBP.attlocal.net>
2021-12-20 13:48:13 -0500

    styled index.css & navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import './Navbar.css'
 
 function Navbar() {
+
+    const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
     return (
         <div className='header'>
-            <Link to='/'>Temporary</Link>
+            <Link to='/'><h1>Temporary</h1></Link>
             <ul className='nav-menu'>
                 <li>
                     <Link to='/'>Home</Link>
                 </li>
                 <li>
-                    <Link to='/'>About Me</Link>
+                    <Link to='/about'>About</Link>
                 </li>
                 <li>
-                    <Link to='/'>Projects</Link>
+                    <Link to='/projects'>Projects</Link>
                 </li>
                 <li>
-                    <Link to='/'>Contact</Link>
+                    <Link to='/contact'>Contact</Link>
                 </li>
             </ul>
             <div className='hamburger'>
-                <FaBars size={20} style={{color:'black'}}/>
+                <FaBars size={20} style={{color:'#fff'}}/>
             </div>
         </div>
     )
